b73669115589e154ab292b05509babb48b96b5d4
itsnot-aduck <hoangduc1662002@gmail.com>
2024-04-18 23:53:22 +0700

    edit

--- custom-addons/real_estate_ads/models/property_offer.py
+++ custom-addons/real_estate_ads/models/property_offer.py
@@ -12,25 +12,25 @@ class PropertyOffer(models.Model):
     partner_id = fields.Many2one('res.partner', string="Customer")
     property_id = fields.Many2one('estate.property', string="Property")
     validity = fields.Integer(string="Validity")
-    creation_date = fields.Date(string="Create Date", default = '_set_create_date')
-
-   
-    deadline = fields.Date(string="Deadline", compute='_compute_deadline', inverse='_inverse_deadline')
-    @api.depends('creation_date', 'validity')
-    def _compute_deadline(self):
-        for rec in self:
-            if rec.creation_date:
-                rec.deadline = rec.creation_date + timedelta(days=rec.validity)
-            else: 
-                rec.deadline = False
-    def _inverse_deadline(self):
-        if rec.deadline and rec.creation_date:
-            for rec in self:
-                rec.validity = (rec.deadline - rec.creation_date).days
-        else:
-            rec.validity = False
-
 
     @api.model
     def _set_create_data(self):
         return fields.Date.today()
+    creation_date = fields.Date(string="Create Date", default = _set_create_data)
+
+    @api.depends('creation_date', 'validity')
+    def _compute_deadline(self):
+        for rec in self:
+            if rec.creation_date and rec.validity:
+                rec.deadline = rec.creation_date + timedelta(days=rec.validity)
+            else: 
+                rec.deadline = False
+    def _inverse_deadline(self):
+        for rec in self:
+            rec.validity = (rec.deadline - rec.creation_date).days
+    
+    deadline = fields.Date(string="Deadline", compute='_compute_deadline', inverse='_inverse_deadline')
+    
+
+    
+    
